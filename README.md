2c2c4dba7f1286c3383e47c87c20aaf75e51d8ee
prajwal raju <prajwalrajup@gmail.com>
2021-04-12 03:37:18 +0530

    added documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# TicTacToe
+A Tic Tac Toe game built for java console.
+
+Run the Main.java file in src to launch the game
+
+# Demo
+<img width="896" alt="Screenshot 2021-04-12 at 2 45 23 AM" src="https://user-images.githubusercontent.com/57785107/114321635-7303c480-9b39-11eb-8d23-4e7433d4f921.png">
--- src/com/prajwal/Main.java
+++ src/com/prajwal/Main.java
@@ -3,11 +3,14 @@ package com.prajwal;
 import java.util.*;
 
 public class Main {
-
+    //Array list to hold player and cpu positions
     static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
     static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
+    // Main method
     public static void main(String[] args) {
 
+        // Creating a 2D array to store the game Board
         char [] [] gameBoard = {{'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'},
                                 {'|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'},
                                 {'|', '-', '-', '-', '+', '-', '-', '-', '+', '-', '-', '-', '|'},
@@ -16,62 +19,109 @@ public class Main {
                                 {'|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', ' ', '|'},
                                 {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'}};
 
+        // Print the empty Game Board
         printGame(gameBoard);
+
+        // Loop through the game untill someone wins the game
         while (true) {
+
+            ////////////// Player's Turn ///////////////////
+
+            // Ask Player to input the position in which the x has to be placed on game Board
             System.out.println("Player's Turn");
-            Scanner scan = new Scanner(System.in);
             System.out.print("Enter the next position : ");
+            Scanner scan = new Scanner(System.in);
+
+            // Store the player position in variable playerPos
             var playerPos = scan.nextInt();
+
+            // Ask player to enter a different position if the position entered is already occupied
             while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)){
                 System.out.print("Position taken, Chose a different position : ");
                 playerPos = scan.nextInt();
             }
 
+            // Update the gameBoard and show it to the users
             updatePos(gameBoard, playerPos, "player");
-
             printGame(gameBoard);
             System.out.println();
 
+            // Check if there are any winners
             var results = checkWinner();
+
+            // If any Users have won exit the loop
             if (results.length() > 0) {
                 System.out.println(results);
                 break;
             }
 
+            ////////////// CPU's Turn ///////////////////
+
+            // generate a random position to place O on the game Board
             System.out.println(" CPU's Turn");
             Random rand = new Random();
             var cpuPos = rand.nextInt(9) + 1;
+
+            // make cpu to use a different position if the position is already occupied
             while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){
                 cpuPos = rand.nextInt(9) + 1;
             }
+
+            // Update the gameBoard and show it to the users
             updatePos(gameBoard, cpuPos, "cpu");
             printGame(gameBoard);
             System.out.println();
+
+            // Check if there are any winners
             results = checkWinner();
+
+            // If any Users have won exit the loop
             if (results.length() > 0) {
                 System.out.println(results);
                 break;
             }
         }
     }
+
+    // Method to Print the game Board
     public static void printGame(char [][] gameBoard){
+
+        // Loop through each ROW in the gameBoard
         for(char [] row : gameBoard){
+
+            // Loop through each item in the ROW
             for(char c : row){
+
+                // Print each item in the ROW
                 System.out.print(c);
             }
+
+            // Print a new line at the end of a ROW
             System.out.println();
         }
     }
+
+    // Method to update the user position on the Game Board
     public static void updatePos(char [][] gameBoard, int pos, String user){
+
+        // Declaring the symbol
         var symbol = ' ';
+
+        // Assign the proper symbol based on the user
         if(user.equals("player")){
             symbol = 'X';
+
+            // add the position of the player to the playerPosition array
             playerPositions.add(pos);
         }
         else if(user.equals("cpu")){
             symbol = 'O';
+
+            // add the position of the CPU to the cpuPosition array
             cpuPositions.add(pos);
         }
+
+        // insert the appropriate symbol in the gameBoard based on the user Position
         switch (pos){
             case 1:
                 gameBoard[1][2] = symbol;
@@ -100,11 +150,17 @@ public class Main {
             case 9:
                 gameBoard[5][10] = symbol;
                 break;
+
+            // break if default
             default:
                 break;
         }
     }
+
+    // Method to chek is any user has won the game
     public static String checkWinner(){
+
+        // declare the winning conditions
         var topRow = Arrays.asList(1,2,3);
         var midRow = Arrays.asList(4,5,6);
         var botRow = Arrays.asList(7,8,9);
@@ -114,6 +170,7 @@ public class Main {
         var dig1 = Arrays.asList(1,5,9);
         var dig2 = Arrays.asList(3,5,7);
 
+        // create a list of winning conditions
         var winning = new ArrayList<List>();
         winning.add(topRow);
         winning.add(midRow);
@@ -124,13 +181,20 @@ public class Main {
         winning.add(dig1);
         winning.add(dig2);
 
+        // loop through the winning conditions
         for (var l : winning){
+
+            // check if the Player has satisfied any winning conditions
             if(playerPositions.containsAll(l) ){
                 return "************** \nCongrats u won \n**************";
             }
+
+            // check if the CPU has satisfied any winning conditions
             else if(cpuPositions.containsAll(l)){
                 return "************* \n  u lost :( \n*************";
             }
+
+            // if all the positions are filled in the gameBoard declare it as a Draw match
             else if(playerPositions.size() + cpuPositions.size() == 9){
                 return "************* \n    Draw \n*************";
             }
